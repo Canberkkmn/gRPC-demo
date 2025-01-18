9b51c7f7b90ab12bc6149a4d08ed235a781c400e
Canberkkmn <canberkkaman@gmail.com>
2025-01-19 01:56:22 +0300

    change startup project to groomserver

--- groomserver/obj/Debug/net9.0/groomserver.AssemblyInfo.cs
+++ groomserver/obj/Debug/net9.0/groomserver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("groomserver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e266edbde2aa28cfcde626e6613652717adf36be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f1a86ac7b5b66909246c039d84c7c81ab2ce42")]
 [assembly: System.Reflection.AssemblyProductAttribute("groomserver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("groomserver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
