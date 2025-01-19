95cc7b05fe9977140de99cbc6c25c440ac377792
Canberkkmn <canberkkaman@gmail.com>
2025-01-19 03:37:44 +0300

    add newsbot

--- groomclient/obj/Debug/net9.0/groomclient.AssemblyInfo.cs
+++ groomclient/obj/Debug/net9.0/groomclient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("groomclient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64dcfed8bec970cfabc95b08e458d7b980ec10dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e2afd52b0f14db9ce3e778e751c9d24af9f867")]
 [assembly: System.Reflection.AssemblyProductAttribute("groomclient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("groomclient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
